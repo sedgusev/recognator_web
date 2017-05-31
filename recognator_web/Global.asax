b97f027ab2333b9d8bd2105b6f91bab0093943b3
Sergey Gusev <sedgusev@outlook.com>
2017-05-31 14:08:17 +0400

    Add project files.

---
+++ recognator_web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="recognator_web.MvcApplication" Language="C#" %>
---
+++ recognator_web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(recognator_web.Startup))]
+namespace recognator_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
